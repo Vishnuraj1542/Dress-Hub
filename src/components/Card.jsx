a164ffa7f169b553dcb0986247ce792fd9557fe1
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-09-27 22:40:02 +0530

    third commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+export default function Card(){
+    return(
+        <div className="card-parent bg-secondary w-80">
+            <div className="card-img w-40 h-60">
+                <img src="/im.jpg" alt="image of dress" className="w-full h-full object-cover"></img>
+            </div>
+            <div className="card-description">
+                <h1>shirt</h1>
+
+            </div>
+            <div className="card-icon">
+
+            </div>
+        </div>
+    )
+
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,12 @@ import {Link} from "react-router-dom"
 
 export default function NavBar(){
     return(
-        <nav className='bg-primary flex flex-row h-12 items-center'>
+        <nav className='bg-background '>
         <h1 className='font-mono text-4xl ml-5'><span className="dark:text-white text-5xl">D</span>ress<span className="dark:text-white">H</span>ub</h1>
         <ul className='flex flex-row gap-4 align ml-auto mr-4 text-xl font-serif'>
-            <li><Link to="/home"> Home </Link></li>
-            <li><Link to='/wishlist'>Wishlist</Link></li>
-            <li><Link to ="/cart">Cart</Link></li>
+            <li className="font-serif"><Link to="/home"> Home </Link></li>
+            <li className="font-serif"><Link to='/wishlist'>Wishlist</Link></li>
+            <li className="font-serif"><Link to ="/cart">Cart</Link></li>
         </ul>
         <div className="flex flex-row gap-4 mr-7">
             <input type='text'className="border-2 border-sky-500 pl-2" placeholder="Search a product...."></input>
