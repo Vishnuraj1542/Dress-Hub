1a1b58b96c73f2d976d4bdaa03282e2aa7ec95c0
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-10-13 23:05:15 +0530

    ux

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,22 @@
 export default function Card(){
     return(
-        <div className="bg-blue-500 flex flex-row ">
-            <div class="flex w-full">
-  <div class="w-32 flex-none bg-red-300">Fixed</div>
-  <div class="flex-grow bg-blue-300">Flexible</div>
-  <div class="basis-[200px] flex-shrink bg-green-300">Shrinkable</div>
-</div>
+        <div className=" flex flex-row flex-wrap justify-center gap-3">
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            
+            
         </div>
     )
 }
+
+
+
+
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,13 @@ export default function NavBar(){
     return(
         <nav className='bg-background min-w-320px flex flex-col lg:h-18 lg:flex-row lg:items-center'>
         <div className="flex flex-row relative gap-4 pt-2 lg:items-center">
-            <h1 className='font-mono text-lg ml-4 lg:text-4xl'><span className="dark:text-white text-2xl lg:text-5xl">D</span>ress<span className="dark:text-white text-2xl lg:text-4xl">H</span>ub</h1>
+            <h1 className='font-mono text-lg ml-3 lg:text-4xl'><span className="dark:text-white text-2xl lg:text-5xl">D</span>ress<span className="dark:text-white text-2xl lg:text-4xl">H</span>ub</h1>
             <FaSearch className="absolute left-33 top-5.5 z-1 text-black-400 lg:left-55 lg:top-6.5"/>
-            <input type='text'className="bg-secondary pl-10 w-45 rounded-lg focus:outline-none focus:ring-2 focus:ring-highlight lg:h-10 lg:w-90 lg:pl-13 md:w-90" placeholder="Search products"></input>
-            <button className='bg-secondary max-w-40 w-md hover:bg-highlight rounded-md mr-4 lg:w-15 h-10 md:w-40'>Login</button>
+            <input type='text'className="bg-secondary pl-10 w-40 rounded-lg focus:outline-none focus:ring-2 focus:ring-highlight lg:h-10 lg:w-90 lg:pl-13 md:w-90" placeholder="Search products"></input>
+            <button className='bg-secondary w-20 hover:bg-highlight rounded lg:w-15 h-10 md:w-40'>Login</button>
         </div>
        
-        <ul className='flex flex-row items-center justify-center gap-10 mr-4 text-md font-serif p-2 ml-2 lg:items-center lg:text-2xl lg:gap-15 md:justify-center md:gap-20 md:text-xl'>
+        <ul className='flex flex-row items-center justify-center gap-10 mr-4 text-md font-serif p-2 ml-2 lg:items-center lg:text-1xl lg:gap-20 md:justify-center md:gap-20 md:text-xl'>
             <li className="font-serif"><Link to="/home"> Home</Link></li>
             <li className="font-serif"><Link to='/wishlist'>Wishlist</Link></li>
             <li className="font-serif"><Link to ="/cart">Cart</Link></li>
