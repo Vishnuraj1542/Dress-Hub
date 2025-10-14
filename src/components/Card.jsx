347da221ac209e01a86294dfd188da4273220973
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-10-14 22:57:58 +0530

    ux

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,102 @@
+import {Heart} from 'lucide-react'
 export default function Card(){
     return(
         <div className=" flex flex-row flex-wrap justify-center gap-3">
-            <div className="container w-43 mt-5 mb-3 rounded-md bg-white">
+            <div className="w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3 relative">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                    <div className="absolute top-2 right-2">
+                        <Heart className='bg-white rounded-xl'/>
+                    </div>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
+                <div className="image m-3">
+                    <img src="im.jpg" alt="" className="rounded-lg"/>
+                </div>
+                <div className="details font-serif bg-">
+                    <p className="text-center">Otto Shirts</p>
+                    <p className="truncate ml-2">This a shirt from the house of Otto with pure cotton material with different colour</p>
+                    <h2 className="pl-2 pb-2">Price: 300</h2>
+                </div>
+            </div>
+            <div className="container w-43 mt-5 mb-3 rounded-md bg-white lg:w-60">
                 <div className="image m-3">
                     <img src="im.jpg" alt="" className="rounded-lg"/>
                 </div>
