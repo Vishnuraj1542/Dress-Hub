b59eee2924aaabb28aa1a891db13707a0eb6a75e
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-10-22 22:12:52 +0530

    created ui details and cart

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,14 +1,17 @@
 export default function Details(){
     return(
-        <div className="border-black w-screen h-100 bg-red-90">
-            <div className='fullview w-80 m-auto mt-4'>
-                <img src='im.jpg' className="w-80 object-fit rounded-xl"></img>
-            </div>
-            <div className="details m-2">
-                <h2 className="text-center font-medium text-xl">Pan America</h2>
-                <p className="text-sans text-xl font-[700]">Color : Red</p>
-                <p className="font-serif text-xl">this is a shirt of otto company which is also used as a partywera</p>
-                <h2 className="text-xl">Price : 500</h2>
+        <div className="container">
+            <div className="item">
+                <div className='fullview w-80 m-auto mt-4 rounded-xl shadow-xl/50 lg:h-60 lg:w-60'>
+                    <img src='im.jpg' className="object-fit rounded-xl lg:h-60"></img>
+                </div>
+
+                <div className="details m-2 align-center m-5 border-2 m">
+                    <h2 className=" text-center font-medium text-xl">Pan America</h2>
+                    <p className="text-sans text-xl font-[700]">Color : Red</p>
+                    <p className="font-serif text-xl">this is a shirt of otto company which is also used as a partywera</p>
+                    <h2 className="text-xl">Price : 500</h2>
+                </div>
                 <div className="flex flex-row gap-9 justify-center mt-5">
                     <span className="border-1 h-7 w-7 rounded-full text-center">S</span>
                     <span className="border-1 h-7 w-7 rounded-full text-center">M</span>
