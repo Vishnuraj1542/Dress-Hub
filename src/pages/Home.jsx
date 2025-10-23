b420653fe5263b6b13057bd4692cfdb19a62fd3b
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-10-23 16:58:03 +0530

    Squashed 'frontend/' content from commit b59eee2
    git-subtree-dir: frontend
    git-subtree-split: b59eee2924aaabb28aa1a891db13707a0eb6a75e

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Card from "../components/Card.jsx"
+export default function Home(){
+    return(
+        <div className='bg-secondary h-100'>
+        <h1 className=''>This is Homepage</h1>
+        <Card/>
+        </div>
+    )
+}
---
+++ src/pages/WishList.jsx
@@ -0,0 +1,9 @@
+import Details from "../components/Detail"
+export default function WishList(){
+    return(
+        <div>
+            <Details/>
+        </div>
+
+    )
+}
