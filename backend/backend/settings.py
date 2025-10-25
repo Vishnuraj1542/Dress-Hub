eff4448d89745cc48a98f9e108976b9c86922e6e
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-10-25 22:18:36 +0530

    backend user registration model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'backend.urls'
 REST_FRAMEWORK ={
     'DEFAULT_AUTHENTICATION_CLASSES':(
     'rest_framework_simplejwt.authentication.JWTAuthentication'),
-    'DEFAULT_PERMISSION_CLASSES':('')
+    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated')
 }
 
 TEMPLATES = [
