39a14db2127c98a440a8c974dc685cf043cd913f
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2025-11-09 23:00:11 +0530

    user details model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,4 +5,12 @@ from django.contrib.auth.models import AbstractUser
 class UserAccount(AbstractUser):
     phone = models.CharField(max_length=12,unique=True)
     email=models.EmailField(unique=True)
-    name=models.CharField(unique=True)
+
+
+class UserDetails(models.Model):
+    name = models.CharField(max_length=22,null=True,blank=True)
+    address=models.TextField(null=True,blank=True)
+    gender=models.CharField(max_length=10,null=True,blank=True)
+    dob=models.DateField(null=True,blank=True)
+    pincode=models.IntegerField(null=True,blank=True)
+    
